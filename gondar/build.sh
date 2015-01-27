1f28210812c00cad8ac702a46210d322f4396914
chennanfei <chennanfei200@163.com>
2015-01-27 03:18:47 +0000

    removed projects

--- gondar/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp -r /workspace/mathildetech/gondar .
-sudo docker build -t index.alauda.io/chennanfei/gondar .
-
-sudo rm -r gondar
--- jakiro/jakiro_internal_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-redis-server &
-
-python manage_internal.py runserver 0.0.0.0:5001
